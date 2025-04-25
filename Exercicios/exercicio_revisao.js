1ca5d2c8ef8833a37f0cb43f3cd71cc8e1a148a6
unknown <pedronascimento0885@digitalcollege.internal>
2025-04-25 16:44:41 -0300

    fuction

--- Exercicios/exercicio_revisao.js
+++ Exercicios/exercicio_revisao.js
@@ -1,46 +1,76 @@
-let nome = "Pedro Juan"
-let idade = 22
-let nacionalidade = "Brasileiro"
+// let nome = "Pedro Juan"
+// let idade = 22
+// let nacionalidade = "Brasileiro"
 
-console.log("Eu me chamo " + nome + " e tenho " + idade + " anos e sou " + nacionalidade);
+// console.log("Eu me chamo " + nome + " e tenho " + idade + " anos e sou " + nacionalidade);
 
-console.log("");
-console.log("");
-console.log("");
+// console.log("");
+// console.log("");
+// console.log("");
 
-let eAluno = true
-let curso = "full Stack"
+// let eAluno = true
+// let curso = "full Stack"
 
-if (eAluno === true) {
-    console.log("Olá, me chamo " + nome + " e sou aluno do curso " + curso);
+// if (eAluno === true) {
+//     console.log("Olá, me chamo " + nome + " e sou aluno do curso " + curso);
+// }
+
+// else { console.log("Olá, me chamo " + nome + " e sou professor do curso " + curso); }
+
+// let frutas = ["laranja", "Maça", "uva" , "goiaba" , "manga" , "kiui" , "jaca" , "morango" , "uva verde" , "tomate"]
+
+// console.log(frutas[0]);
+
+// console.log(frutas.length);
+
+// console.log(frutas[frutas.length -1]);
+
+
+// console.log("");
+// console.log("");
+// console.log("");
+
+
+// for (let i = 1; i<= 10; i++){
+//     console.log(i);
+// }
+
+
+
+// console.log("");
+// console.log("");
+// console.log("");
+
+// for( let i = 1; i < frutas.length; i++){
+//     console.log(frutas[i]);
+// }
+
+
+// function saudaçao() {
+//     console.log("Olá Mundo");   
+// }
+
+// saudaçao();
+
+// function soma(numero1 , numero2) {
+//     console.log(numero1 + numero2);
+//     console.log(numero1);
+//     console.log(numero2);
+
+//     let soma = numero1 + numero2
+
+//     console.log("soma: " + soma);
+
+    
+// }
+
+// soma(4 ,4)
+
+
+
+function saudaçao(nome1, idade , nacionalidade) {
+    console.log("ola " + nome1 + " e vc tem " + idade + " anos de idade e sua nacionalidade é: " + nacionalidade);
+    
 }
 
-else { console.log("Olá, me chamo " + nome + " e sou professor do curso " + curso); }
-
-let frutas = ["laranja", "Maça", "uva" , "goiaba" , "manga" , "kiui" , "jaca" , "morango" , "uva verde" , "tomate"]
-
-console.log(frutas[0]);
-
-console.log(frutas.length);
-
-console.log(frutas[frutas.length -1]);
-
-
-console.log("");
-console.log("");
-console.log("");
-
-
-for (let i = 1; i<= 10; i++){
-    console.log(i);
-}
-
-
-
-console.log("");
-console.log("");
-console.log("");
-
-for( let i = 1; i < frutas.length; i++){
-    console.log(frutas[i]);
-}
+saudaçao("joao" , 10 , "Brasileiro")
