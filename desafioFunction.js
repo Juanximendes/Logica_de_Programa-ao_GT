131a50177c212640d31648f79b845d31a793860a
juanximendes <juanximendes00@gmail.com>
2025-05-07 16:44:55 -0300

    atualizado

--- desafioFunction.js
+++ desafioFunction.js
@@ -1,121 +1,131 @@
-function saudaçao1() {
-    console.log("Olá, seja bem-vindo!");
-}
+// function saudaçao1() {
+//     console.log("Olá, seja bem-vindo!");
+// }
 
-saudaçao1();
-
-
-function saudaçao(nome) {
-    console.log("Olá " + nome + "!");
-
-}
-
-saudaçao("João");
+// saudaçao1();
 
 
-function soma(numero1, numero2) {
-    let soma = numero1 + numero2
+// function saudaçao(nome) {
+//     console.log("Olá " + nome + "!");
 
-    console.log("soma: " + soma);
+// }
 
-}
-
-soma(1, 2)
+// saudaçao("João");
 
 
+// function soma(numero1, numero2) {
+//     let soma = numero1 + numero2
 
-function par(numero) {
+//     console.log("soma: " + soma);
 
-    if (numero % 2 === 0) {
-        console.log("O numero é par")
-    }
+// }
 
-    else { console.log("O numero é impar") }
-}
-
-par(2)
-
-function media(nota1, nota2, nota3) {
-
-    let notaM = (nota1 + nota2 + nota3) / 3
-    console.log("Sua media é: " + notaM + " e você foi:  ");
-
-    if (notaM >= 7) { console.log("Aprovado") }
-
-    else if (notaM >= 5 && notaM < 7) { console.log("recuperaçao") }
-
-    else { console.log("Reprovado") }
-
-}
-
-media(10, 10, 10)
+// soma(1, 2)
 
 
-function dobro(nu) { let nume = (nu + nu); console.log(nume); }
 
-dobro(10)
+// function par(numero) {
 
+//     if (numero % 2 === 0) {
+//         console.log("O numero é par")
+//     }
 
-function maior(number1, number2) {
+//     else { console.log("O numero é impar") }
+// }
 
-    if (number1 > number2) {
-        console.log(number1);
-    }
+// par(2)
 
-    else if (number1 === number2) {
-        console.log("Os números são Iguais");
+// function media(nota1, nota2, nota3) {
 
+//     let notaM = (nota1 + nota2 + nota3) / 3
+//     console.log("Sua media é: " + notaM + " e você foi:  ");
 
-    }
+//     if (notaM >= 7) { console.log("Aprovado") }
 
-    else { console.log(number2) }
-}
+//     else if (notaM >= 5 && notaM < 7) { console.log("recuperaçao") }
 
-maior(20, 20)
+//     else { console.log("Reprovado") }
 
+// }
 
-function saudarUsuario(nome, hora) {
-    if (hora < 12) {
-        return `Bom dia, ${nome}!`;
-    } else if (hora>=12 && hora < 18) {
-        return `Boa tarde, ${nome}!`;
-    } else {
-        return `Boa noite, ${nome}!`;
-    }
-}
-
-console.log(saudarUsuario("Pedro", 19));
+// media(10, 10, 10)
 
 
-console.log("");
+// function dobro(nu) { let nume = (nu + nu); console.log(nume); }
+
+// dobro(10)
 
 
-function gerarRecibo(nome, produto, valor) {
-    return `Recibo de Compra
-  Cliente: ${nome}
-  Produto: ${produto}
-  Total: R$${valor}
-  Obrigado pela preferência!`;
-}
+// function maior(number1, number2) {
 
-console.log(gerarRecibo("Juan", "Geladeira", 3500));
+//     if (number1 > number2) {
+//         console.log(number1);
+//     }
 
-console.log("");
+//     else if (number1 === number2) {
+//         console.log("Os números são Iguais");
 
-function emailBoasVindas(nome, curso) {
-    return `Olá, ${nome}!
-  Seja bem-vindo ao curso de ${curso}.
-  Estamos felizes por tê-lo conosco!`;
-}
 
-console.log(emailBoasVindas("Pedro", "Programação em JavaScript"));
+//     }
 
-console.log("");
+//     else { console.log(number2) }
+// }
+
+// maior(20, 20)
+
+// console.log("");
+
+
+// function saudarUsuario(nome, hora) {
+//     if (hora < 12) {
+//         return `Bom dia, ${nome}!`;
+//     } else if (hora>=12 && hora < 18) {
+//         return `Boa tarde, ${nome}!`;
+//     } 
+//     else if (hora>= 18 && hora <=24 ) {
+//         return ` Boa noite, ${nome} `
+        
+//     }
+//     else {
+//         return `O horario: ${nome} é invalido!`;
+//     }
+// }
+
+// console.log(saudarUsuario("Pedro", 19));
+
+
+// console.log("");
+
+
+// function gerarRecibo(nome, produto, valor) {
+//     return `Recibo de Compra
+//   Cliente: ${nome}
+//   Produto: ${produto}
+//   Total: R$${valor}
+//   Obrigado pela preferência!`;
+// }
+
+// console.log(gerarRecibo("Juan", "Geladeira", 3500));
+
+// console.log("");
+
+// function emailBoasVindas(nome, curso) {
+//     return `Olá, ${nome}!
+//   Seja bem-vindo ao curso de ${curso}.
+//   Estamos felizes por tê-lo conosco!`;
+// }
+
+// console.log(emailBoasVindas("Pedro", "FullStack"));
+
+// console.log("");
 
 
 function calcularDesconto(nome, preco, desconto) {
-    return `Olá, ${nome}. O produto de R$${preco} com desconto de ${desconto}% custa R$${(preco * (1 - desconto / 100)).toFixed(2)}.`;
+    const desconto1 = preco * (desconto / 100);
+    return `Olá, ${nome}. O produto custa: R$${preco} e com desconto de ${desconto1}% custa: R$${preco - desconto1}.`;
 }
+
+console.log(calcularDesconto("Pedro", 200, 10));
 
 
 
